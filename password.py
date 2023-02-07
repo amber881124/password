32ef34394611421d6a2f0e1be1d2a4104a71d8f2
momo <zyongzhu24@gmail.com>
2023-02-07 12:33:02 +0800

    advance

--- password.py
+++ password.py
@@ -1,10 +1,13 @@
 my_password = 'a123456'
+most = 3
 x = 1
-while x <= 3:
+while x <= most:
     password = input('請輸入密碼： ')
     if password == my_password:
         print('登入成功!')
         break
     else:
-        print(f'密碼錯誤! 還有{3-x}次機會')
+        print('密碼錯誤!')
+        if most-x > 0:
+            print(f'還有{most-x}次機會')
         x += 1
